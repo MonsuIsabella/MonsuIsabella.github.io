6489f9074429f455cfcbbd3c52f5f362ff1b0c63
MonsuIsabella <139663716+MonsuIsabella@users.noreply.github.com>
2023-07-15 23:04:26 -0300

    Add files via upload

---
+++ OPEN EDITORS/public/app.js
@@ -0,0 +1,13 @@
+/* Cuando hago CLICK .button, .nav TOGGLE "activo" */
+const button = document.querySelector(".button")
+const nav = document.querySelector(".nav")
+
+function addEventListener(){
+    let(click.button)
+}
+function classList(){
+    toggle("activo")
+}
+button.addEventListener ("click" ,()=>{
+    nav.classList.toggle("activo")
+})
